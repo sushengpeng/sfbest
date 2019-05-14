2141cd33be26e3a70c392f8e17e1a87683011a8e
局外人 <15539550863@163.com>
2019-05-14 19:44:31 +0800

    细节修改

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,6 @@
 //  <!-- 商品轮播图 -->
 var swiper = new Swiper('.swiper-container1', {
-    // spaceBetween: 30,
+    // spaceBetween: 80,
 });
 //  < !--同类推荐 -->
 var swiper = new Swiper('.swiper-container2', {
@@ -60,43 +60,30 @@ function getRequest(str) {
             let box = information[0].img.split(',')
             box.map(res=>{
                 imgurl += `
-                <div class="swiper-slide adver1 swiper-slide1 swiper-slide-active" style="width: 358px;">
+                
+                <div class="swiper-slide swiper-slide1">
                 <img src=${res}>
                 </div>
                 `
             })
             $('.wrapper').html(imgurl)
             $('.p-price .sp1')[0].innerText = '￥' +information[0].price
-            console.log(information);
-            
             $('.p-place-country')[0].innerText = information[0].field
             $('.p-name')[0].innerText = information[0].itemname
             $('.p-info-price')[0].innerText = information[0].comnum
             $('.spc')[0].innerText = information[0].weight
-            // $('.p-price .sp1')
-            // console.log($('.swiper-slide1 img').attr('src'));
-            // $('.swiper-slide1 img').each(function(){
-            //     $(this).attr('src',information[0].img)
-            // })
-            // console.log(information[0].img.split(','));
-            // if(box.length==1){
-            //     console.log(true);
-            // }
-            
-            
-            
         }
     })
 }()
-!function(){
-    let conunt=0;
-    $('.addshoppingcart').tap(function(){
-        // conunt=conunt++;
-        console.log(111);   
-    })
-    $('.cart-bg span')[0].innerText = conunt
+// !function(){
+//     let conunt=0;
+//     $('.addshoppingcart').tap(function(){
+//         // conunt=conunt++;
+//         console.log(111);   
+//     })
+//     $('.cart-bg span')[0].innerText = conunt
     
-}()
+// }()
 
 
 
