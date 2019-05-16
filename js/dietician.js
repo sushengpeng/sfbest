0e3bfc0ebb189e167eab7f4365c98260b2aec267
Hmm <mohy@example.com>
2019-05-16 20:05:09 +0800

    hmm

--- js/dietician.js
+++ js/dietician.js
@@ -41,4 +41,17 @@ mui.ajax('../php/class.php',
    }
 )
 
-console.log(11111)
+mui.init({
+   pullRefresh : {
+     container:"补钙",//待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
+     up : {
+       height:30,//可选.默认50.触发上拉加载拖动距离
+       auto:true,//可选,默认false.自动上拉加载一次
+       contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
+       contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
+       callback :pullfresh-function(){
+         this.endPullupToRefresh(true|false);
+       } ,//必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+     }
+   }
+ });
